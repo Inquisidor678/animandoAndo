13241c77433df6438217614020777d65fec13647
Your Name <you@example.com>
2020-11-20 00:52:15 -0500

    animacion condicional

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#btnPalpitar').click(function() {
+        const tiene = $('.hola').hasClass('saludo');
+
+        if (tiene == true) {
+            $('.hola').removeClass('saludo');
+        } else {
+            $('.hola').addClass('saludo');
+        }
+
+    })
+})
